05990fe0ee59c97076f2689f13c546011b7e2699
Zilya <zilya.nurislamova@mail.ru>
2022-07-05 17:08:33 +0300

    String to Integer (atoi)

--- src/main/java/com/leetcode/string/to/integer/atoi/Solution.java
+++ src/main/java/com/leetcode/string/to/integer/atoi/Solution.java
@@ -35,3 +35,23 @@ public class Solution {
         }
     }
 }
+
+//// Traverse next digits of input and stop if it is not a digit
+//        while (index < n && Character.isDigit(input.charAt(index))) {
+//        int digit = input.charAt(index) - '0';
+//
+//        // Check overflow and underflow conditions.
+//        if ((result > Integer.MAX_VALUE / 10) ||
+//        (result == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
+//        // If integer overflowed return 2^31-1, otherwise if underflowed return -2^31.
+//        return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+//        }
+//
+//        // Append current digit to the result.
+//        result = 10 * result + digit;
+//        index++;
+//        }
+//
+//        // We have formed a valid number without any overflow/underflow.
+//        // Return it after multiplying it with its sign.
+//        return sign * result;
