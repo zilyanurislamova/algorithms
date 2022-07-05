68f9a2a8ce220a134d1f6be45d47b541de35ea26
Zilya <zilya.nurislamova@mail.ru>
2022-07-05 15:26:41 +0300

    Reverse Integer

---
+++ src/main/java/reverse/integer/Solution.java
@@ -0,0 +1,23 @@
+package reverse.integer;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(reverse(-120));
+    }
+
+    public static int reverse(int x) {
+        if (x == 0)
+            return 0;
+        StringBuilder result = x >= 0 ? new StringBuilder("") : new StringBuilder("-");
+        while (Math.abs(x) % 10 > 0 || Math.abs(x) / 10 > 0) {
+            result.append(Math.abs(x) % 10);
+            x = Math.abs(x) / 10;
+        }
+        try {
+            return Integer.parseInt(String.valueOf(result));
+        }
+        catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+}
